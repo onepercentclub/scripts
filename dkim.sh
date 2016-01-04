eeb1f2d00123440d22e97a63d53d9bca17d65456
Ernst Odolphi <ernst@onepercentclub.com>
2016-01-04 13:11:34 +0100

    Small formatting fixes for python output

--- dkim.sh
+++ dkim.sh
@@ -9,12 +9,13 @@ openssl genrsa -out $DOMAIN.priv 1024
 openssl rsa -in $DOMAIN.priv -out $DOMAIN.pub -pubout -outform PEM
 
 # One line the public key
-PUB_KEY=`tr -d '\n' < $DOMAIN.pub | sed "s/^-----BEGIN PUBLIC KEY-----//" | sed "s/-----END PUBLIC KEY-----$//"`
+PUB_KEY=`tr -d '\n' < $DOMAIN.pub | sed "s/^-----BEGIN PUBLIC KEY-----\n//" | sed "s/-----END PUBLIC KEY-----$//"`
 
 # Display server properties
-PRIV_KEY=`cat $DOMAIN.priv | sed 's/^/\"/' | sed 's/$/\\\<n>\" \\\/'`
-echo "\n====\nServer Properties (Note: replace the <n> below with an n)\n===="
-echo "DKIM_PRIVATE_KEY = $PRIV_KEY"
+PRIV_KEY=`cat $DOMAIN.priv | sed 's/^/    \"/' | sed 's/$/\"/'`
+echo "\n====\nServer Properties\n===="
+echo "DKIM_PRIVATE_KEY = (\n$PRIV_KEY
+)"
 
 
 echo "DKIM_DOMAIN = \"$DOMAIN\""
